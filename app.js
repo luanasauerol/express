9936066bb271f2386d66b711d0ab9cae9b7bc69e
luanasauerol <luana.sauer@mercadolivre.com>
2022-07-22 17:08:35 -0300

    express exercicio 2

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 const express = require('express');
-// import express from 'express';
-
-// const server = express();
 const app = express();
+let produtos = require('./produtos');
 
-// primeiro parâmetro ('/') URL da rota
-// segundo parâmetro  callback com dois parâmetros ((req,res))
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
 
-    // em caso de erro, podemos redirecionar o usuário para uma página de erro
-    // res.redirect('/erro');
-})
+app.get('/produtos', (req, res) => {
+    res.json(produtos);
+});
 
-app.listen(3000, () => {
+app.post('/produtos', (req, res) => {
+    const content = req.body;
+    produtos = [...produtos, ...content];
+    res.status(201).json(produtos);
+});
+
+app.put('/produtos/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const content = req.body;
+    const produtoEncontrado = produtos.find((produto) => produto.id === id);
+
+    if (!produtoEncontrado) {
+        return res.status(400).json({ "message": "Produto não encontrado" });
+    }
+    const produtoAtualizado = produtos.map((produto) => {
+        if (produto.id === id) {
+            return content;
+        }
+        return produto;
+    })
+    produtos = produtoAtualizado;
+    res.status(200).json(produtos);
+
+});
+
+
+app.delete('/produtos/:id', (req, res)=>{
+    const id = Number(req.params.id);
+    const produtoEncontrado = produtos.find((produto) => produto.id === id);
+    if (!produtoEncontrado) {
+        return res.status(400).json({ "message": "Produto não encontrado" });
+    }
+
+    produtos = produtos.filter((produto)=>produto.id !== id);
+    res.status(200).json(produtos);
+});
+
+app.listen(3001, () => {
     console.log('Servidor em execução');
 });
